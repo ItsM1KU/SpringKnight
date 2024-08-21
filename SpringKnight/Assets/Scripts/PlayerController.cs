eb3ed51c9b058f4f685f7906a9cf23fd1a9bc129
Sanjay <sanjayvarma44433@gmail.com>
2024-08-21 23:20:36 +0530

    revamp

--- SpringKnight/Assets/Scripts/PlayerController.cs
+++ SpringKnight/Assets/Scripts/PlayerController.cs
@@ -4,95 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpSpeed = 8f;
-    private float direction = 0f;
-    private Rigidbody2D player;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float movespeed = 5f;
+    [SerializeField] float jumpspeed = 2f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
 
-    public Transform groundCheck;
-    public float groundCheckRadius;
-    public LayerMask groundLayer;
-    private bool isTouchingGround;
+    private float horizontal;
 
-    public float jumpStartTime;
-    private float jumpTime;
-    private bool isJumping;
-
-    public Transform wallCheck;
-    public LayerMask wallLayer;
-    private bool isWallSliding;
-    private float wallslidespeed = 0.2f;
-
-    void Start()
+    private void Update()
     {
-        player = GetComponent<Rigidbody2D>();
+        horizontal = Input.GetAxisRaw("Horizontal");
+        jump();
     }
 
-
-    void Update()
+    private void FixedUpdate()
     {
-        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        direction = Input.GetAxis("Horizontal");
-
-        if (direction > 0f)
-        {
-            player.velocity = new Vector2(direction * speed, player.velocity.y);
-        }
-        else if (direction < 0f)
-        {
-            player.velocity = new Vector2(direction * speed, player.velocity.y);
-        }
-        else
-        {
-            player.velocity = new Vector2(0, player.velocity.y);
-        }
-
-        if (Input.GetButtonDown("Jump") && isTouchingGround)
-        {
-            isJumping = true;
-            jumpTime = jumpStartTime;
-            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
-        }
-
-        if(Input.GetButton("Jump") && isTouchingGround)
-        {
-            if (jumpTime > 0)
-            {
-                player.velocity = new Vector2(player.velocity.x, jumpSpeed);
-                jumpTime -= Time.deltaTime;
-            }
-            else
-            {
-                isJumping = false;
-            }
-        }
-
-        if (Input.GetButtonUp("Jump"))
-        {
-            isJumping = false;
-        }
-
-        wallSlide();
+        rb.velocity = new Vector2(horizontal * movespeed, rb.velocity.y);
     }
 
-    private bool isWalled()
+    void jump()
     {
-        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
-    }
-
-    private void wallSlide()
-    {
-        if( isWalled() && !isTouchingGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
-            isWallSliding = true;
-            player.velocity = new Vector2(player.velocity.x, Mathf.Clamp(player.velocity.y, -wallslidespeed, float.MaxValue));
-        }
-        else
-        {
-            isWallSliding = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
         }
     }
-        
 
-        
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
 }
